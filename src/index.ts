246c8bc00b79f2226b0cfc959dbbe40ee6e92bfc
Dat Nguyen <nguyentiendat12c@gmail.com>
2025-06-11 02:15:55 +0700

    chore: test lint-staged and completed setup eslint, prettier

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ const PORT = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
