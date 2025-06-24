58bf03666f40116acd98e3a189e0bc52f186c376
Dat Nguyen <nguyentiendat12c@gmail.com>
2025-06-24 10:09:13 +0700

    chore(deps): remove all remnants of library eslint, prettier

--- src/api-docs/openAPIDocumentGenerator.ts
+++ src/api-docs/openAPIDocumentGenerator.ts
@@ -1,19 +1,19 @@
-import { OpenAPIRegistry, OpenApiGeneratorV3 } from '@asteasolutions/zod-to-openapi';
+import { OpenAPIRegistry, OpenApiGeneratorV3 } from "@asteasolutions/zod-to-openapi";
 
-import { userRegistry } from '../features/user/userRouter';
+import { userRegistry } from "../features/user/userRouter";
 
-export type OpenAPIDocument = ReturnType<OpenApiGeneratorV3['generateDocument']>;
+export type OpenAPIDocument = ReturnType<OpenApiGeneratorV3["generateDocument"]>;
 
 export const generateOpenApiDocument = (): OpenAPIDocument => {
-  const registry = new OpenAPIRegistry([userRegistry]);
-  const generator = new OpenApiGeneratorV3(registry.definitions);
+	const registry = new OpenAPIRegistry([userRegistry]);
+	const generator = new OpenApiGeneratorV3(registry.definitions);
 
-  return generator.generateDocument({
-    openapi: '3.1.0',
-    info: {
-      title: 'TS Express Starter',
-      version: '1.0.0',
-    },
-    servers: [{ url: 'http://localhost:3000' }],
-  });
+	return generator.generateDocument({
+		openapi: "3.1.0",
+		info: {
+			title: "TS Express Starter",
+			version: "1.0.0",
+		},
+		servers: [{ url: "http://localhost:3000" }],
+	});
 };
